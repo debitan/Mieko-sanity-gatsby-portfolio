36c4e3712fbb2816fd0c27f2df3b6c8572a986ea
David Matthews <david.matthews@digital.trade.gov.uk>
2019-08-10 22:21:16 +0100

    Fix SVG so site can build:

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -5,7 +5,6 @@ import SEO from '../components/seo'
 import Layout from '../containers/layout'
 import BlockText from '../components/block-text'
 import styled from 'styled-components'
-import linkedIn from '../images/linkedIn.svg'
 
 export const query = graphql`
   query AboutPageQuery {
@@ -53,8 +52,7 @@ const AboutPage = props => {
     }
   `
 
-  const LinkedIn = styled.img`
-    height: 6vh;
+  const LinkedIn = styled.svg`
     display: block;
     margin: auto;
   `
@@ -66,7 +64,7 @@ const AboutPage = props => {
         <div>
           <BlockText blocks={site._rawAbout} />
           <a href='https://www.linkedin.com/in/mieko-h-36a02b188/'>
-            <LinkedIn src={linkedIn} alt='LinkedIn logo' />
+            <LinkedIn xmlns='http://www.w3.org/2000/svg' width='8vh' viewBox='0 0 24 24'><path d='M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z' /></LinkedIn>
           </a>
         </div>
       </Hero>
